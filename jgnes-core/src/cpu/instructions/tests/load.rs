cea780291bf0123f9f898895eaa3c604218ca9e9
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-04-22 23:50:36 -0500

    more store tests

--- jgnes-core/src/cpu/instructions/tests/load.rs
+++ jgnes-core/src/cpu/instructions/tests/load.rs
@@ -36,6 +36,8 @@ fn lda_immediate() {
     );
 }
 
+// TODO LDA zero page / zero page X / absolute / absolute X / absolute Y / indexed indirect / indirect indexed
+
 #[test]
 fn ldx_immediate() {
     run_test(
@@ -72,6 +74,8 @@ fn ldx_immediate() {
     );
 }
 
+// TODO LDX zero page / zero page Y / absolute / absolute Y
+
 #[test]
 fn ldy_immediate() {
     run_test(
@@ -107,6 +111,8 @@ fn ldy_immediate() {
         },
     );
 }
+
+// TODO LDY zero page / zero page X / absolute / absolute X
 
 #[test]
 fn tax() {
@@ -403,7 +409,7 @@ fn sta_absolute() {
     );
 
     run_test(
-        // LDA #$85; STA 6578
+        // LDA #$85; STA $6578
         concat!("A985", "8D7865"),
         ExpectedState {
             a: Some(0x85),
@@ -494,6 +500,200 @@ fn sta_absolute_y() {
             p: Some(0x34),
             memory: hash_map! { 0x65F0: 0x00, 0x6624: 0x85 },
             cycles: Some(11),
+            ..ExpectedState::default()
+        },
+    );
+}
+
+// TODO STA indexed indirect / indirect indexed
+
+#[test]
+fn stx_zero_page() {
+    run_test(
+        // STX $45
+        "8645",
+        ExpectedState {
+            x: Some(0x00),
+            p: Some(0x34),
+            memory: hash_map! { 0x0045: 0x00 },
+            cycles: Some(3),
+            ..ExpectedState::default()
+        },
+    );
+
+    run_test(
+        // LDX #$BA; STX $45
+        concat!("A2BA", "8645"),
+        ExpectedState {
+            x: Some(0xBA),
+            p: Some(0xB4),
+            memory: hash_map! { 0x0045: 0xBA },
+            cycles: Some(6),
+            ..ExpectedState::default()
+        },
+    );
+}
+
+#[test]
+fn stx_zero_page_indexed() {
+    run_test(
+        // STX $45,Y
+        "9645",
+        ExpectedState {
+            x: Some(0x00),
+            y: Some(0x00),
+            p: Some(0x34),
+            memory: hash_map! { 0x0045: 0x00 },
+            cycles: Some(4),
+            ..ExpectedState::default()
+        },
+    );
+
+    run_test(
+        // LDX #$78; LDY #$05; STX $45,Y
+        concat!("A278", "A005", "9645"),
+        ExpectedState {
+            x: Some(0x78),
+            y: Some(0x05),
+            p: Some(0x34),
+            memory: hash_map! { 0x0045: 0x00, 0x004A: 0x78 },
+            cycles: Some(10),
+            ..ExpectedState::default()
+        },
+    );
+
+    run_test(
+        // LDX #$78; LDY #$10; STX $F5,Y
+        concat!("A278", "A010", "96F5"),
+        ExpectedState {
+            x: Some(0x78),
+            y: Some(0x10),
+            p: Some(0x34),
+            memory: hash_map! { 0x0005: 0x78, 0x0105: 0x00 },
+            cycles: Some(10),
+            ..ExpectedState::default()
+        },
+    );
+}
+
+#[test]
+fn stx_absolute() {
+    run_test(
+        // STX $6578
+        "8E7865",
+        ExpectedState {
+            x: Some(0x00),
+            p: Some(0x34),
+            memory: hash_map! { 0x6578: 0x00 },
+            cycles: Some(4),
+            ..ExpectedState::default()
+        },
+    );
+
+    run_test(
+        // LDX #$85; STX $6578
+        concat!("A285", "8E7865"),
+        ExpectedState {
+            x: Some(0x85),
+            p: Some(0xB4),
+            memory: hash_map! { 0x6578: 0x85 },
+            cycles: Some(7),
+            ..ExpectedState::default()
+        },
+    );
+}
+
+#[test]
+fn sty_zero_page() {
+    run_test(
+        // STY $45
+        "8445",
+        ExpectedState {
+            y: Some(0x00),
+            p: Some(0x34),
+            memory: hash_map! { 0x0045: 0x00 },
+            cycles: Some(3),
+            ..ExpectedState::default()
+        },
+    );
+
+    run_test(
+        // LDY #$BA; STY $45
+        concat!("A0BA", "8445"),
+        ExpectedState {
+            y: Some(0xBA),
+            p: Some(0xB4),
+            memory: hash_map! { 0x0045: 0xBA },
+            cycles: Some(6),
+            ..ExpectedState::default()
+        },
+    );
+}
+
+#[test]
+fn sty_zero_page_indexed() {
+    run_test(
+        // STY $45,X
+        "9445",
+        ExpectedState {
+            y: Some(0x00),
+            x: Some(0x00),
+            p: Some(0x34),
+            memory: hash_map! { 0x0045: 0x00 },
+            cycles: Some(4),
+            ..ExpectedState::default()
+        },
+    );
+
+    run_test(
+        // LDY #$78; LDX #$05; STY $45,X
+        concat!("A078", "A205", "9445"),
+        ExpectedState {
+            y: Some(0x78),
+            x: Some(0x05),
+            p: Some(0x34),
+            memory: hash_map! { 0x0045: 0x00, 0x004A: 0x78 },
+            cycles: Some(10),
+            ..ExpectedState::default()
+        },
+    );
+
+    run_test(
+        // LDY #$78; LDX #$10; STY $F5,X
+        concat!("A078", "A210", "94F5"),
+        ExpectedState {
+            y: Some(0x78),
+            x: Some(0x10),
+            p: Some(0x34),
+            memory: hash_map! { 0x0005: 0x78, 0x0105: 0x00 },
+            cycles: Some(10),
+            ..ExpectedState::default()
+        },
+    );
+}
+
+#[test]
+fn sty_absolute() {
+    run_test(
+        // STY $6578
+        "8C7865",
+        ExpectedState {
+            y: Some(0x00),
+            p: Some(0x34),
+            memory: hash_map! { 0x6578: 0x00 },
+            cycles: Some(4),
+            ..ExpectedState::default()
+        },
+    );
+
+    run_test(
+        // LDY #$85; STY $6578
+        concat!("A085", "8C7865"),
+        ExpectedState {
+            y: Some(0x85),
+            p: Some(0xB4),
+            memory: hash_map! { 0x6578: 0x85 },
+            cycles: Some(7),
             ..ExpectedState::default()
         },
     );
