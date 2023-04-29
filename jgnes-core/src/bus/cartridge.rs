f59ef499b9e99255bdde09c59916c10681ed6f5c
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-04-29 18:59:49 -0500

    move iNES header parsing to its own function

--- jgnes-core/src/bus/cartridge.rs
+++ jgnes-core/src/bus/cartridge.rs
@@ -189,7 +189,7 @@ pub enum CartridgeFileError {
     #[error("invalid or unsupported file format")]
     Format,
     #[error("unsupported mapper: {mapper_number}")]
-    UnsupportedMapper { mapper_number: u8 },
+    UnsupportedMapper { mapper_number: u16 },
     #[error("cartridge header specifies both volatile and non-volatile PRG RAM")]
     MultiplePrgRamTypes,
 }
@@ -222,114 +222,159 @@ enum FileFormat {
     Nes2Point0,
 }
 
+#[derive(Debug, Clone)]
+struct INesHeader {
+    mapper_number: u16,
+    sub_mapper_number: u8,
+    prg_rom_size: u32,
+    prg_ram_size: u32,
+    chr_rom_size: u32,
+    chr_ram_size: u32,
+    chr_type: ChrType,
+    nametable_mirroring: NametableMirroring,
+    has_trainer: bool,
+    has_battery: bool,
+}
+
+impl INesHeader {
+    fn parse_from_file(file: &mut File) -> Result<INesHeader, CartridgeFileError> {
+        file.seek(SeekFrom::Start(0))?;
+
+        let mut header = [0; 16];
+        file.read_exact(&mut header)?;
+
+        // All iNES headers should begin with this 4-byte sequence, which is "NES" followed by the
+        // character that MS-DOS used for EOF
+        if header[..4] != [0x4E, 0x45, 0x53, 0x1A] {
+            return Err(CartridgeFileError::Format);
+        }
+
+        let prg_rom_size = 16 * 1024 * ((u32::from(header[9] & 0x0F) << 8) | u32::from(header[4]));
+        let chr_rom_size = 8 * 1024 * ((u32::from(header[9] & 0xF0) << 4) | u32::from(header[5]));
+
+        let has_trainer = header[6] & 0x04 != 0;
+
+        let mapper_number = u16::from((header[7] & 0xF0) | ((header[6] & 0xF0) >> 4));
+
+        let chr_type = if chr_rom_size == 0 {
+            ChrType::RAM
+        } else {
+            ChrType::ROM
+        };
+
+        let nametable_mirroring = if header[6] & 0x01 != 0 {
+            NametableMirroring::Vertical
+        } else {
+            NametableMirroring::Horizontal
+        };
+
+        // TODO make persistent save files work
+        let has_battery = header[6] & 0x02 != 0;
+
+        let format = if header[7] & 0x0C == 0x08 {
+            FileFormat::Nes2Point0
+        } else {
+            FileFormat::INes
+        };
+
+        let sub_mapper_number = match format {
+            FileFormat::Nes2Point0 => header[8] >> 4,
+            FileFormat::INes => 0,
+        };
+
+        let prg_ram_size = match format {
+            FileFormat::Nes2Point0 => {
+                let volatile_shift = header[10] & 0x0F;
+                let non_volatile_shift = header[10] >> 4;
+                // TODO separate these? very very few games have both volatile and non-volatile RAM
+                let shift = volatile_shift + non_volatile_shift;
+                if shift > 0 {
+                    64 << shift
+                } else {
+                    0
+                }
+            }
+            FileFormat::INes => 8192,
+        };
+
+        let chr_ram_size = match (chr_type, format) {
+            (ChrType::RAM, FileFormat::Nes2Point0) => {
+                let chr_ram_shift = header[11] & 0x0F;
+                if chr_ram_shift > 0 {
+                    64 << chr_ram_shift
+                } else {
+                    0
+                }
+            }
+            (ChrType::RAM, FileFormat::INes) => 8192,
+            (ChrType::ROM, _) => 0,
+        };
+
+        Ok(Self {
+            mapper_number,
+            sub_mapper_number,
+            prg_rom_size,
+            prg_ram_size,
+            chr_rom_size,
+            chr_ram_size,
+            chr_type,
+            nametable_mirroring,
+            has_trainer,
+            has_battery,
+        })
+    }
+}
+
 fn from_ines_file(mut file: File) -> Result<Mapper, CartridgeFileError> {
-    file.seek(SeekFrom::Start(0))?;
+    let header = INesHeader::parse_from_file(&mut file)?;
 
-    let mut header = [0; 16];
-    file.read_exact(&mut header)?;
+    // Header is 16 bytes, trainer is 512 bytes if present
+    let prg_rom_start_address = if header.has_trainer { 16 + 512 } else { 16 };
 
-    let prg_rom_size = 16 * 1024 * ((u32::from(header[9] & 0x0F) << 8) | u32::from(header[4]));
-    let chr_rom_size = 8 * 1024 * ((u32::from(header[9] & 0xF0) << 4) | u32::from(header[5]));
-
-    let has_trainer = header[6] & 0x04 != 0;
-
-    let mapper_number = (header[7] & 0xF0) | ((header[6] & 0xF0) >> 4);
-
-    let prg_rom_start_address = if has_trainer { 16 + 512 } else { 16 };
-
-    let mut prg_rom = vec![0; prg_rom_size as usize];
+    let mut prg_rom = vec![0; header.prg_rom_size as usize];
     file.seek(SeekFrom::Start(prg_rom_start_address))?;
     file.read_exact(&mut prg_rom)?;
 
-    let mut chr_rom = vec![0; chr_rom_size as usize];
+    let mut chr_rom = vec![0; header.chr_rom_size as usize];
     file.read_exact(&mut chr_rom)?;
-
-    let chr_type = if chr_rom_size == 0 {
-        ChrType::RAM
-    } else {
-        ChrType::ROM
-    };
-
-    let nametable_mirroring = if header[6] & 0x01 != 0 {
-        NametableMirroring::Vertical
-    } else {
-        NametableMirroring::Horizontal
-    };
-
-    // TODO make persistent save files work
-    let has_battery = header[6] & 0x02 != 0;
-
-    let format = if header[7] & 0x0C == 0x08 {
-        FileFormat::Nes2Point0
-    } else {
-        FileFormat::INes
-    };
-
-    let sub_mapper_number = match format {
-        FileFormat::Nes2Point0 => header[8] >> 4,
-        FileFormat::INes => 0,
-    };
-
-    let prg_ram_size = match format {
-        FileFormat::Nes2Point0 => {
-            let volatile_shift = header[10] & 0x0F;
-            let non_volatile_shift = header[10] >> 4;
-            // TODO separate these? very very few games have both volatile and non-volatile RAM
-            let shift = volatile_shift + non_volatile_shift;
-            if shift > 0 {
-                64 << shift
-            } else {
-                0
-            }
-        }
-        FileFormat::INes => 8192,
-    };
-
-    let chr_ram_size = match (chr_type, format) {
-        (ChrType::RAM, FileFormat::Nes2Point0) => {
-            let chr_ram_shift = header[11] & 0x0F;
-            if chr_ram_shift > 0 {
-                64 << chr_ram_shift
-            } else {
-                0
-            }
-        }
-        (ChrType::RAM, FileFormat::INes) => 8192,
-        (ChrType::ROM, _) => 0,
-    };
-
-    let chr_size = match chr_type {
-        ChrType::ROM => chr_rom.len(),
-        ChrType::RAM => chr_ram_size,
-    };
 
     let cartridge = Cartridge {
         prg_rom,
-        prg_ram: vec![0; prg_ram_size],
+        prg_ram: vec![0; header.prg_ram_size as usize],
         chr_rom,
-        chr_ram: vec![0; chr_ram_size],
+        chr_ram: vec![0; header.chr_ram_size as usize],
     };
 
-    let mapper = match mapper_number {
+    let chr_size = match header.chr_type {
+        ChrType::ROM => header.chr_rom_size,
+        ChrType::RAM => header.chr_ram_size,
+    };
+
+    let mapper = match header.mapper_number {
         0 => Mapper::Nrom(MapperImpl {
             cartridge,
-            data: Nrom::new(chr_type, nametable_mirroring),
+            data: Nrom::new(header.chr_type, header.nametable_mirroring),
         }),
         1 => Mapper::Mmc1(MapperImpl {
             cartridge,
-            data: Mmc1::new(chr_type),
+            data: Mmc1::new(header.chr_type),
         }),
         2 => Mapper::Uxrom(MapperImpl {
             cartridge,
-            data: Uxrom::new(chr_type, nametable_mirroring),
+            data: Uxrom::new(header.chr_type, header.nametable_mirroring),
         }),
         3 => Mapper::Cnrom(MapperImpl {
             cartridge,
-            data: Cnrom::new(chr_type, nametable_mirroring),
+            data: Cnrom::new(header.chr_type, header.nametable_mirroring),
         }),
         4 => Mapper::Mmc3(MapperImpl {
             cartridge,
-            data: Mmc3::new(chr_type, prg_rom_size, chr_size as u32, sub_mapper_number),
+            data: Mmc3::new(
+                header.chr_type,
+                header.prg_rom_size,
+                chr_size,
+                header.sub_mapper_number,
+            ),
         }),
         5 => Mapper::Mmc5(MapperImpl {
             cartridge,
@@ -337,22 +382,32 @@ fn from_ines_file(mut file: File) -> Result<Mapper, CartridgeFileError> {
         }),
         7 => Mapper::Axrom(MapperImpl {
             cartridge,
-            data: Axrom::new(chr_type),
+            data: Axrom::new(header.chr_type),
         }),
         _ => {
-            return Err(CartridgeFileError::UnsupportedMapper { mapper_number });
+            return Err(CartridgeFileError::UnsupportedMapper {
+                mapper_number: header.mapper_number,
+            });
         }
     };
 
-    log::info!("Mapper number: {mapper_number} ({})", mapper.name());
-    log::info!("PRG ROM size: {prg_rom_size}");
-    log::info!("PRG RAM size: {prg_ram_size}");
-    log::info!("Cartridge has battery-backed PRG RAM: {has_battery}");
-    log::info!("CHR ROM size: {chr_rom_size}");
-    log::info!("CHR RAM size: {chr_ram_size}");
-    log::info!("CHR memory type: {chr_type:?}");
     log::info!(
-        "Hardwired nametable mirroring: {nametable_mirroring:?} (not applicable to all mappers)"
+        "Mapper number: {} ({})",
+        header.mapper_number,
+        mapper.name()
+    );
+    log::info!("PRG ROM size: {}", header.prg_rom_size);
+    log::info!("PRG RAM size: {}", header.prg_ram_size);
+    log::info!(
+        "Cartridge has battery-backed PRG RAM: {}",
+        header.has_battery
+    );
+    log::info!("CHR ROM size: {}", header.chr_rom_size);
+    log::info!("CHR RAM size: {}", header.chr_ram_size);
+    log::info!("CHR memory type: {:?}", header.chr_type);
+    log::info!(
+        "Hardwired nametable mirroring: {:?} (not applicable to all mappers)",
+        header.nametable_mirroring
     );
 
     Ok(mapper)
