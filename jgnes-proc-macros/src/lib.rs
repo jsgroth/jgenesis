43bfa7cafb97e598840998d63ed04e4c6c04f719
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-05-22 20:59:58 -0500

    repurpose the EnumDisplay and EnumFromStr macros I wrote for jgb to replace some hand-written Display and FromStr impls

--- jgnes-proc-macros/src/lib.rs
+++ jgnes-proc-macros/src/lib.rs
@@ -126,3 +126,98 @@ pub fn match_each_variant_macro(input: TokenStream) -> TokenStream {
 
     gen.into()
 }
+
+/// Implement the `std::fmt::Display` trait for the given enum. Only supports enums which have only
+/// fieldless variants.
+///
+/// # Panics
+///
+/// This macro will panic if applied to a struct, a union, or an enum with any variants that have
+/// fields.
+#[proc_macro_derive(EnumDisplay)]
+pub fn enum_display(input: TokenStream) -> TokenStream {
+    let ast: DeriveInput = syn::parse(input).expect("unable to parse input");
+
+    let name = &ast.ident;
+
+    let Data::Enum(data) = &ast.data
+        else {
+            panic!("EnumDisplay derive macro can only be applied to enums; {name} is not an enum");
+        };
+
+    let match_arms: Vec<_> = data
+        .variants
+        .iter()
+        .map(|variant| {
+            let variant_name = &variant.ident;
+            assert!(variant.fields.is_empty(), "EnumDisplay macro only supports enums with only fieldless variants; {name}::{variant_name} has fields");
+
+            let variant_name_str = variant_name.to_string();
+            quote! {
+                Self::#variant_name => write!(f, #variant_name_str)
+            }
+        })
+        .collect();
+
+    let gen = quote! {
+        impl ::std::fmt::Display for #name {
+            fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+                match self {
+                    #(#match_arms,)*
+                }
+            }
+        }
+    };
+
+    gen.into()
+}
+
+/// Implement the `std::str::FromStr` trait for the given enum, with `FromStr::Err` set to `String`.
+/// Only supports enums which have only fieldless variants. The generated implementation will be
+/// case-insensitive.
+///
+/// # Panics
+///
+/// This macro will panic if applied to a struct, a union, or an enum with any variants that have
+/// fields.
+#[proc_macro_derive(EnumFromStr)]
+pub fn enum_from_str(input: TokenStream) -> TokenStream {
+    let ast: DeriveInput = syn::parse(input).expect("unable to parse input");
+
+    let name = &ast.ident;
+
+    let Data::Enum(data) = &ast.data
+        else {
+            panic!("EnumFromStr derive macro can only be applied to enums; {name} is not an enum");
+        };
+
+    let match_arms: Vec<_> = data
+        .variants
+        .iter()
+        .map(|variant| {
+            let variant_name = &variant.ident;
+            assert!(variant.fields.is_empty(), "EnumFromStr macro only supports enums with only fieldless variants; {name}::{variant_name} has fields");
+
+            let variant_name_lowercase = variant_name.to_string().to_ascii_lowercase();
+            quote! {
+                #variant_name_lowercase => Ok(Self::#variant_name)
+            }
+        })
+        .collect();
+
+    let err_fmt_string = format!("invalid {name} string: '{{}}'");
+    let gen = quote! {
+        impl ::std::str::FromStr for #name {
+            type Err = ::std::string::String;
+
+            fn from_str(s: &str) -> ::std::result::Result<Self, Self::Err> {
+                match s.to_ascii_lowercase().as_str() {
+                    #(#match_arms,)*
+                    _ => Err(format!(#err_fmt_string, s))
+                }
+            }
+        }
+    };
+
+    gen.into()
+}
