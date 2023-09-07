8157f7dd75f9868ce895366fe2f9eb4f15f8ed46
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-09-07 01:54:32 -0500

    move WgpuRenderer to its own crate

--- jgenesis-native-driver/src/lib.rs
+++ jgenesis-native-driver/src/lib.rs
@@ -1,7 +1,5 @@
 pub mod config;
 pub mod input;
 mod mainloop;
-mod renderer;
 
 pub use mainloop::{create_genesis, create_smsgg, NativeEmulator, NativeTickEffect};
-pub use renderer::config::{FilterMode, PrescaleFactor, RendererConfig, VSyncMode, WgpuBackend};
