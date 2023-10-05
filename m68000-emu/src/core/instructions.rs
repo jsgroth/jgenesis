7a72a48bc93d608917fbad4c09e7ac3c3298b82a
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-10-05 18:34:00 -0500

    remove commented-out line of code

--- m68000-emu/src/core/instructions.rs
+++ m68000-emu/src/core/instructions.rs
@@ -327,7 +327,7 @@ impl<'registers, 'bus, B: BusInterface> InstructionExecutor<'registers, 'bus, B>
         let opcode = self.fetch_operand()?;
         self.opcode = opcode;
 
-        let instruction = table::decode(opcode); //decode_opcode(opcode, self.registers.supervisor_mode)?;
+        let instruction = table::decode(opcode);
         self.instruction = Some(instruction);
         log::trace!(
             "[{}] Decoded opcode {opcode:04X} (PC={initial_pc:06X}): {instruction}",
