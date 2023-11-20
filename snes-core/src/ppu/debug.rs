26262dbd3287674f7e31b078835b55ddaaf11978
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-11-20 03:03:46 -0600

    don't let brightness affect CGRAM debug window because some games set brightness to 0 in between frames

--- snes-core/src/ppu/debug.rs
+++ snes-core/src/ppu/debug.rs
@@ -3,9 +3,8 @@ use jgenesis_common::frontend::Color;
 
 impl Ppu {
     pub fn debug_cram(&self, out: &mut [Color]) {
-        let brightness = self.registers.brightness;
         for (out_color, &snes_color) in out.iter_mut().zip(self.cgram.iter()) {
-            *out_color = convert_snes_color(snes_color, brightness);
+            *out_color = convert_snes_color(snes_color, 15);
         }
     }
 }
