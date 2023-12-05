a0990550c3808e09fac287f1e75c0383154beca1
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-12-05 17:54:05 -0600

    add comment noting that leap year determination is intentionally not correct

--- backend/snes-coprocessors/src/time.rs
+++ backend/snes-coprocessors/src/time.rs
@@ -18,11 +18,9 @@ pub fn days_in_month(month: u8, year: u8) -> u8 {
         1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
         4 | 6 | 9 | 11 => 30,
         2 => {
-            if year % 4 == 0 {
-                29
-            } else {
-                28
-            }
+            // This is not strictly accurate (it doesn't account for not divisible by 100 or divisible
+            // by 400) but it matches what the RTC chips do
+            if year % 4 == 0 { 29 } else { 28 }
         }
         _ => {
             log::error!("Invalid month: {month}, defaulting to 31 days in month");
