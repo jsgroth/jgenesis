22e0fd1578f63fe80b4fe4aecb85f89b2a3ee77f
jsgroth <1137683+jsgroth@users.noreply.github.com>
2024-01-26 05:46:36 -0600

    [GBC] implement MBC3 real-time clock

--- backend/gb-core/src/cartridge/mappers.rs
+++ backend/gb-core/src/cartridge/mappers.rs
@@ -1,5 +1,9 @@
+pub mod mbc3;
+
+use crate::cartridge::mappers::mbc3::Mbc3Rtc;
 use crate::cartridge::HasBasicRamMapping;
 use bincode::{Decode, Encode};
+use jgenesis_common::frontend::SaveWriter;
 use jgenesis_common::num::{GetBit, U16Ext};
 
 #[derive(Debug, Clone, Copy, PartialEq, Encode, Decode)]
@@ -167,16 +171,18 @@ pub struct Mbc3 {
     ram_bank: u8,
     ram_addr_mask: u32,
     ram_enabled: bool,
+    rtc: Option<Mbc3Rtc>,
 }
 
 impl Mbc3 {
-    pub fn new(rom_len: u32, ram_len: u32) -> Self {
+    pub fn new(rom_len: u32, ram_len: u32, rtc: Option<Mbc3Rtc>) -> Self {
         Self {
             rom_bank: 0,
             rom_addr_mask: rom_len - 1,
             ram_bank: 0,
             ram_addr_mask: if ram_len != 0 { ram_len - 1 } else { 0 },
             ram_enabled: false,
+            rtc,
         }
     }
 
@@ -196,20 +202,74 @@ impl Mbc3 {
             }
             0x4000..=0x5FFF => {
                 // RAM bank number / RTC register select
-                self.ram_bank = value & 0x03;
+                self.ram_bank = value & 0x0F;
             }
             0x6000..=0x7FFF => {
                 // RTC latch
-                log::warn!("Write to unimplemented RTC latch");
+                if let Some(rtc) = &mut self.rtc {
+                    rtc.write_latch(value);
+                }
             }
             0x8000..=0xFFFF => panic!("Invalid cartridge address: {address:06X}"),
         }
     }
-}
 
-impl HasBasicRamMapping for Mbc3 {
-    fn map_ram_address(&self, address: u16) -> Option<u32> {
-        basic_map_ram_address(self.ram_enabled, address, self.ram_bank.into(), self.ram_addr_mask)
+    pub fn read_ram(&self, address: u16, sram: &[u8]) -> u8 {
+        match self.ram_bank {
+            0x00..=0x03 => {
+                // SRAM
+                basic_map_ram_address(
+                    self.ram_enabled,
+                    address,
+                    self.ram_bank.into(),
+                    self.ram_addr_mask,
+                )
+                .map_or(0xFF, |ram_addr| sram[ram_addr as usize])
+            }
+            0x08..=0x0C => {
+                // RTC registers
+                self.rtc.as_ref().map_or(0xFF, |rtc| rtc.read_register(self.ram_bank))
+            }
+            _ => 0xFF,
+        }
+    }
+
+    pub fn write_ram(&mut self, address: u16, value: u8, sram: &mut [u8]) {
+        match self.ram_bank {
+            0x00..=0x03 => {
+                // SRAM
+                let ram_addr = basic_map_ram_address(
+                    self.ram_enabled,
+                    address,
+                    self.ram_bank.into(),
+                    self.ram_addr_mask,
+                );
+                if let Some(ram_addr) = ram_addr {
+                    sram[ram_addr as usize] = value;
+                }
+            }
+            0x08..=0x0C => {
+                // RTC registers
+                if let Some(rtc) = &mut self.rtc {
+                    rtc.write_register(self.ram_bank, value);
+                }
+            }
+            _ => {}
+        }
+    }
+
+    pub fn update_rtc_time(&mut self) {
+        if let Some(rtc) = &mut self.rtc {
+            rtc.update_time();
+        }
+    }
+
+    pub fn save_rtc_state<S: SaveWriter>(&self, save_writer: &mut S) -> Result<(), S::Err> {
+        if let Some(rtc) = &self.rtc {
+            save_writer.persist_serialized("rtc", rtc)?;
+        }
+
+        Ok(())
     }
 }
 
