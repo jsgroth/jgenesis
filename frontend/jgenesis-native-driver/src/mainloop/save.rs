c905802fa85fa3e8e2d1bc1e5505ab3cbfc98b47
jsgroth <1137683+jsgroth@users.noreply.github.com>
2024-03-04 14:19:18 -0600

    more robust save file persistence
    instead of having each write directly overwrite the save file, write first to a temp file and then rename

--- frontend/jgenesis-native-driver/src/mainloop/save.rs
+++ frontend/jgenesis-native-driver/src/mainloop/save.rs
@@ -3,10 +3,10 @@ use bincode::error::{DecodeError, EncodeError};
 use bincode::{Decode, Encode};
 use jgenesis_common::frontend::SaveWriter;
 use std::collections::HashMap;
-use std::fs::{File, OpenOptions};
-use std::io;
+use std::fs::File;
 use std::io::{BufReader, BufWriter, Read, Write};
 use std::path::PathBuf;
+use std::{fs, io};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -14,6 +14,25 @@ pub enum SaveWriteError {
     #[error("Error writing save file to '{path}': {source}")]
     OpenFile {
         path: String,
+        #[source]
+        source: io::Error,
+    },
+    #[error("Error reading save bytes from '{path}': {source}")]
+    ReadFile {
+        path: String,
+        #[source]
+        source: io::Error,
+    },
+    #[error("Error writing save bytes to '{path}': {source}")]
+    WriteFile {
+        path: String,
+        #[source]
+        source: io::Error,
+    },
+    #[error("Failed to rename temp file '{temp_path}' to '{path}': {source}")]
+    RenameFile {
+        path: String,
+        temp_path: String,
         #[source]
         source: io::Error,
     },
@@ -31,89 +50,112 @@ pub enum SaveWriteError {
     },
 }
 
+struct SavePaths {
+    path: PathBuf,
+    temp_path: PathBuf,
+}
+
 pub struct FsSaveWriter {
     base_path: PathBuf,
-    extension_to_path: HashMap<String, PathBuf>,
+    extension_to_paths: HashMap<String, SavePaths>,
 }
 
 impl FsSaveWriter {
     pub fn new(path: PathBuf) -> Self {
-        Self { base_path: path, extension_to_path: HashMap::new() }
+        Self { base_path: path, extension_to_paths: HashMap::new() }
     }
 
-    fn open_file(
-        &mut self,
-        extension: &str,
-        options: &mut OpenOptions,
-    ) -> Result<(File, &PathBuf), SaveWriteError> {
-        if !self.extension_to_path.contains_key(extension) {
+    fn get_or_insert_paths(&mut self, extension: &str) -> &SavePaths {
+        // Double get necessary to avoid borrow checker issues related to returning a reference
+        if !self.extension_to_paths.contains_key(extension) {
             let path = self.base_path.with_extension(extension);
-            self.extension_to_path.insert(extension.into(), path);
+            let temp_path = self.base_path.with_extension(format!("{extension}.tmp"));
+            self.extension_to_paths.insert(extension.into(), SavePaths { path, temp_path });
         }
 
-        let path = &self.extension_to_path[extension];
+        &self.extension_to_paths[extension]
+    }
 
-        let file = options.open(path).map_err(|source| SaveWriteError::OpenFile {
+    fn read_file<D, F>(&mut self, extension: &str, read_fn: F) -> Result<D, SaveWriteError>
+    where
+        F: FnOnce(BufReader<File>, &PathBuf) -> Result<D, SaveWriteError>,
+    {
+        let SavePaths { path, .. } = self.get_or_insert_paths(extension);
+
+        let file = File::open(path).map_err(|source| SaveWriteError::OpenFile {
             path: path.display().to_string(),
             source,
         })?;
-
-        Ok((file, path))
+        let reader = BufReader::new(file);
+        read_fn(reader, path)
     }
-}
 
-macro_rules! file_read_options {
-    () => {
-        File::options().read(true)
-    };
-}
+    fn write_file<F>(&mut self, extension: &str, write_fn: F) -> Result<(), SaveWriteError>
+    where
+        F: FnOnce(BufWriter<File>, &PathBuf) -> Result<(), SaveWriteError>,
+    {
+        let SavePaths { path, temp_path } = self.get_or_insert_paths(extension);
 
-macro_rules! file_write_options {
-    () => {
-        File::options().write(true).create(true).truncate(true)
-    };
+        {
+            let temp_file =
+                File::options().write(true).create(true).truncate(true).open(temp_path).map_err(
+                    |source| SaveWriteError::OpenFile {
+                        path: temp_path.display().to_string(),
+                        source,
+                    },
+                )?;
+            let temp_writer = BufWriter::new(temp_file);
+            write_fn(temp_writer, temp_path)?;
+        }
+
+        fs::rename(temp_path, path).map_err(|source| SaveWriteError::RenameFile {
+            path: path.display().to_string(),
+            temp_path: temp_path.display().to_string(),
+            source,
+        })?;
+
+        Ok(())
+    }
 }
 
 impl SaveWriter for FsSaveWriter {
     type Err = SaveWriteError;
 
     fn load_bytes(&mut self, extension: &str) -> Result<Vec<u8>, Self::Err> {
-        let (file, path) = self.open_file(extension, file_read_options!())?;
-        let mut reader = BufReader::new(file);
-        let mut bytes = Vec::new();
-        reader.read_to_end(&mut bytes).map_err(|source| SaveWriteError::OpenFile {
-            path: path.display().to_string(),
-            source,
-        })?;
-
-        Ok(bytes)
+        self.read_file(extension, |mut reader, path| {
+            let mut bytes = Vec::new();
+            reader.read_to_end(&mut bytes).map_err(|source| SaveWriteError::ReadFile {
+                path: path.display().to_string(),
+                source,
+            })?;
+            Ok(bytes)
+        })
     }
 
     fn persist_bytes(&mut self, extension: &str, bytes: &[u8]) -> Result<(), Self::Err> {
-        let (file, path) = self.open_file(extension, file_write_options!())?;
-        let mut writer = BufWriter::new(file);
-        writer.write_all(bytes).map_err(|source| SaveWriteError::OpenFile {
-            path: path.display().to_string(),
-            source,
-        })?;
-
-        Ok(())
+        self.write_file(extension, |mut writer, path| {
+            writer.write_all(bytes).map_err(|source| SaveWriteError::WriteFile {
+                path: path.display().to_string(),
+                source,
+            })
+        })
     }
 
     fn load_serialized<D: Decode>(&mut self, extension: &str) -> Result<D, Self::Err> {
-        let (file, path) = self.open_file(extension, file_read_options!())?;
-        let mut reader = BufReader::new(file);
-        bincode::decode_from_std_read(&mut reader, bincode_config!())
-            .map_err(|source| SaveWriteError::Decode { path: path.display().to_string(), source })
+        self.read_file(extension, |mut reader, path| {
+            bincode::decode_from_std_read(&mut reader, bincode_config!()).map_err(|source| {
+                SaveWriteError::Decode { path: path.display().to_string(), source }
+            })
+        })
     }
 
     fn persist_serialized<E: Encode>(&mut self, extension: &str, data: E) -> Result<(), Self::Err> {
-        let (file, path) = self.open_file(extension, file_write_options!())?;
-        let mut writer = BufWriter::new(file);
-        bincode::encode_into_std_write(data, &mut writer, bincode_config!()).map_err(|source| {
-            SaveWriteError::Encode { path: path.display().to_string(), source }
-        })?;
+        self.write_file(extension, |mut writer, path| {
+            bincode::encode_into_std_write(data, &mut writer, bincode_config!()).map_err(
+                |source| SaveWriteError::Encode { path: path.display().to_string(), source },
+            )?;
 
-        Ok(())
+            Ok(())
+        })
     }
 }
