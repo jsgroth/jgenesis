1a5b35e6399210da228ed3670258c355eb8f18ed
jsgroth <1137683+jsgroth@users.noreply.github.com>
2024-06-12 03:03:26 -0500

    [32X] implement PWM audio output

--- backend/s32x-core/src/pwm.rs
+++ backend/s32x-core/src/pwm.rs
@@ -1,9 +1,14 @@
 //! 32X PWM sound chip
 
+use crate::audio::PwmResampler;
 use crate::registers::SystemRegisters;
 use bincode::{Decode, Encode};
+use jgenesis_common::frontend::TimingMode;
 use jgenesis_common::num::GetBit;
 use std::collections::VecDeque;
+
+// 53.693175 MHz * 3 / 7 / (1047 - 1) ~= 22 KHz
+const TWENTY_TWO_KHZ_CYCLE_REGISTER: u16 = 1047;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default, Encode, Decode)]
 pub enum OutputDirection {
@@ -128,7 +133,9 @@ pub struct PwmChip {
     l_output: u16,
     r_output: u16,
     cycle_counter: u64,
+    off_cycle_counter: u64,
     timer_counter: u16,
+    genesis_mclk_frequency: f64,
 }
 
 // Cycle register and pulse width are unsigned 12-bit values
@@ -150,7 +157,7 @@ macro_rules! impl_write_register {
 }
 
 impl PwmChip {
-    pub fn new() -> Self {
+    pub fn new(timing_mode: TimingMode) -> Self {
         Self {
             control: PwmControl::new(),
             cycle_register: 0,
@@ -158,16 +165,49 @@ impl PwmChip {
             r_fifo: PwmFifo::new(),
             l_output: U12_MASK,
             r_output: U12_MASK,
-            timer_counter: 16,
             cycle_counter: U12_MASK.into(),
+            off_cycle_counter: U12_MASK.into(),
+            timer_counter: 16,
+            genesis_mclk_frequency: match timing_mode {
+                TimingMode::Ntsc => genesis_core::audio::NTSC_GENESIS_MCLK_FREQUENCY,
+                TimingMode::Pal => genesis_core::audio::PAL_GENESIS_MCLK_FREQUENCY,
+            },
         }
     }
 
-    pub fn tick(&mut self, mut sh2_cycles: u64, system_registers: &mut SystemRegisters) {
-        if self.control.l_out.is_off() && self.control.r_out.is_off() {
+    pub fn tick(
+        &mut self,
+        mut sh2_cycles: u64,
+        system_registers: &mut SystemRegisters,
+        pwm_resampler: &mut PwmResampler,
+    ) {
+        if (self.control.l_out.is_off() && self.control.r_out.is_off()) || self.cycle_register == 1
+        {
             // PWM counters are stopped when both channels are off
+            // Output 0 samples at ~22 KHz
+            pwm_resampler.update_source_frequency(compute_sample_rate(
+                self.genesis_mclk_frequency,
+                TWENTY_TWO_KHZ_CYCLE_REGISTER,
+            ));
+
+            while sh2_cycles != 0 {
+                let prev_cycle_counter = self.off_cycle_counter;
+                self.off_cycle_counter = self.off_cycle_counter.saturating_sub(sh2_cycles);
+                sh2_cycles -= prev_cycle_counter - self.off_cycle_counter;
+
+                if self.off_cycle_counter == 0 {
+                    self.off_cycle_counter = (TWENTY_TWO_KHZ_CYCLE_REGISTER - 1).into();
+                    pwm_resampler.collect_sample(0.0, 0.0);
+                }
+            }
+
             return;
         }
+
+        pwm_resampler.update_source_frequency(compute_sample_rate(
+            self.genesis_mclk_frequency,
+            self.cycle_register,
+        ));
 
         while sh2_cycles != 0 {
             let prev_cycle_counter = self.cycle_counter;
@@ -178,10 +218,20 @@ impl PwmChip {
                 // Cycle counter is always set to (register - 1), wrapping from 0 to 4095
                 self.cycle_counter = (self.cycle_register.wrapping_sub(1) & U12_MASK).into();
 
-                // TODO only pop one FIFO when only one channel is on?
-                // TODO play sound
                 self.l_output = self.l_fifo.pop().unwrap_or(self.l_output);
                 self.r_output = self.r_fifo.pop().unwrap_or(self.r_output);
+
+                let sample_l = match (self.control.l_out, self.control.r_out) {
+                    (OutputDirection::Same, _) => pulse_width_to_f64(self.l_output),
+                    (_, OutputDirection::Opposite) => pulse_width_to_f64(self.r_output),
+                    _ => 0.0,
+                };
+                let sample_r = match (self.control.r_out, self.control.l_out) {
+                    (OutputDirection::Same, _) => pulse_width_to_f64(self.r_output),
+                    (_, OutputDirection::Opposite) => pulse_width_to_f64(self.l_output),
+                    _ => 0.0,
+                };
+                pwm_resampler.collect_sample(sample_l, sample_r);
 
                 self.timer_counter -= 1;
                 if self.timer_counter == 0 {
@@ -265,4 +315,14 @@ impl PwmChip {
         log::trace!("Mono pulse width FIFO write: {value:04X}");
         log::trace!("  Effective wave height: {sample}");
     }
+}
+
+fn compute_sample_rate(genesis_mclk_frequency: f64, cycle_register: u16) -> f64 {
+    genesis_mclk_frequency * 3.0 / 7.0 / f64::from(cycle_register.wrapping_sub(1) & U12_MASK)
+}
+
+// Map from [0, 4096) to [-1.0, 1.0]
+fn pulse_width_to_f64(pulse_width: u16) -> f64 {
+    let divisor = 0.5 * f64::from(U12_MASK);
+    (f64::from(pulse_width) - divisor) / divisor
 }
