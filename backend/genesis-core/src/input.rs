018d5a7689b523bb61e7f8d00dd0110d477c4bf6
jsgroth <1137683+jsgroth@users.noreply.github.com>
2024-06-27 14:15:06 -0500

    [GEN] rework how writes to the controller DATA/CTRL registers affect the controller's inputs select bit; this fixes controls not working properly in Trouble Shooter (#110)

--- backend/genesis-core/src/input.rs
+++ backend/genesis-core/src/input.rs
@@ -39,124 +39,98 @@ pub enum GenesisControllerType {
     SixButton,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Default, Encode, Decode)]
-enum InputPinDirection {
-    #[default]
-    Input,
-    Output,
-}
-
-impl InputPinDirection {
-    fn from_ctrl_bit(bit: bool) -> Self {
-        if bit { Self::Output } else { Self::Input }
-    }
-
-    fn to_ctrl_bit(self) -> bool {
-        self == Self::Output
-    }
-
-    fn to_data_bit(self, joypad_bit: bool, data_bit: bool) -> bool {
-        match self {
-            Self::Input => joypad_bit,
-            Self::Output => data_bit,
-        }
-    }
-}
-
 // Slightly less than 1.5ms
 const FLIP_COUNTER_CYCLES: u32 = 10000;
+
+const TH_BIT: u8 = 6;
 
 #[derive(Debug, Clone, Copy, Default, Encode, Decode)]
 struct PinDirections {
     last_data_write: u8,
+    last_ctrl_write: u8,
     th_flip_count: u8,
     flip_reset_counter: u32,
-    th: InputPinDirection,
-    tr: InputPinDirection,
-    tl: InputPinDirection,
-    right: InputPinDirection,
-    left: InputPinDirection,
-    down: InputPinDirection,
-    up: InputPinDirection,
+    controller_th: bool,
 }
 
 impl PinDirections {
-    fn write_ctrl(&mut self, ctrl_byte: u8) {
-        self.th = InputPinDirection::from_ctrl_bit(ctrl_byte.bit(6));
-        self.tr = InputPinDirection::from_ctrl_bit(ctrl_byte.bit(5));
-        self.tl = InputPinDirection::from_ctrl_bit(ctrl_byte.bit(4));
-        self.right = InputPinDirection::from_ctrl_bit(ctrl_byte.bit(3));
-        self.left = InputPinDirection::from_ctrl_bit(ctrl_byte.bit(2));
-        self.down = InputPinDirection::from_ctrl_bit(ctrl_byte.bit(1));
-        self.up = InputPinDirection::from_ctrl_bit(ctrl_byte.bit(0));
+    fn write_ctrl(&mut self, ctrl_byte: u8, controller_type: GenesisControllerType) {
+        self.last_ctrl_write = ctrl_byte;
+        self.maybe_set_th(controller_type);
     }
 
     fn write_data(&mut self, data_byte: u8, controller_type: GenesisControllerType) {
-        let prev_th = self.th.to_data_bit(true, self.last_data_write.bit(6));
         self.last_data_write = data_byte;
-        let th = self.th.to_data_bit(true, self.last_data_write.bit(6));
+        self.maybe_set_th(controller_type);
+    }
+
+    fn maybe_set_th(&mut self, controller_type: GenesisControllerType) {
+        if !self.last_ctrl_write.bit(TH_BIT) {
+            // TH bit is set to input; writes won't take effect until it's changed back to output
+            return;
+        }
+
+        let th = self.last_data_write.bit(TH_BIT);
 
         // 6-button controller cycles through 4 different modes whenever TH flips from 0 to 1,
         // resetting after ~1.5ms have passed without such a flip
-        if controller_type == GenesisControllerType::SixButton && !prev_th && th {
-            self.th_flip_count = (self.th_flip_count + 1) & 0x03;
+        if controller_type == GenesisControllerType::SixButton && !self.controller_th && th {
+            self.th_flip_count = (self.th_flip_count + 1) % 4;
             self.flip_reset_counter = FLIP_COUNTER_CYCLES;
         }
+        self.controller_th = th;
     }
 
     fn to_data_byte(self, joypad_state: GenesisJoypadState) -> u8 {
-        let th = self.th.to_data_bit(true, self.last_data_write.bit(6));
+        let mut controller_byte = match (self.th_flip_count, self.controller_th) {
+            (0..=2, true) => {
+                // 3-button: B, C, and directional inputs
+                (u8::from(!joypad_state.c) << 5)
+                    | (u8::from(!joypad_state.b) << 4)
+                    | (u8::from(!joypad_state.right) << 3)
+                    | (u8::from(!joypad_state.left) << 2)
+                    | (u8::from(!joypad_state.down) << 1)
+                    | u8::from(!joypad_state.up)
+            }
+            (0..=1, false) => {
+                // 3-button: A and Start (and up/down)
+                (u8::from(!joypad_state.start) << 5)
+                    | (u8::from(!joypad_state.a) << 4)
+                    | (u8::from(!joypad_state.down) << 1)
+                    | u8::from(!joypad_state.up)
+            }
+            (3, true) => {
+                // 6-button: New buttons (and B and C)
+                (u8::from(!joypad_state.c) << 5)
+                    | (u8::from(!joypad_state.b) << 4)
+                    | (u8::from(!joypad_state.mode) << 3)
+                    | (u8::from(!joypad_state.x) << 2)
+                    | (u8::from(!joypad_state.y) << 1)
+                    | u8::from(!joypad_state.z)
+            }
+            (2, false) => {
+                // 6-button: A, Start, and all 0s in the lower bits
+                (u8::from(!joypad_state.start) << 5) | (u8::from(!joypad_state.a) << 4)
+            }
+            (3, false) => {
+                // 6-button: A, Start, and all 1s in the lower bits
+                (u8::from(!joypad_state.start) << 5) | (u8::from(!joypad_state.a) << 4) | 0b00001111
+            }
+            _ => panic!("th_flip_count should always be <= 3, was {}", self.th_flip_count),
+        };
+        controller_byte |= u8::from(self.controller_th) << 6;
 
-        let tr_joypad = if th { !joypad_state.c } else { !joypad_state.start };
-        let tl_joypad = if th { !joypad_state.b } else { !joypad_state.a };
-        let d3_joypad = match (self.th_flip_count, th) {
-            (0..=2, true) => !joypad_state.right,
-            (3, true) => !joypad_state.mode,
-            (0..=2, false) => false,
-            (3, false) => true,
-            _ => panic!("th_flip_count should always be <= 3"),
-        };
-        let d2_joypad = match (self.th_flip_count, th) {
-            (0..=2, true) => !joypad_state.left,
-            (3, true) => !joypad_state.x,
-            (0..=2, false) => false,
-            (3, false) => true,
-            _ => panic!("th_flip_count should always be <= 3"),
-        };
-        let d1_joypad = match (self.th_flip_count, th) {
-            (0 | 1, _) | (2, true) => !joypad_state.down,
-            (3, true) => !joypad_state.y,
-            (2, false) => false,
-            (3, false) => true,
-            _ => panic!("th_flip_count should always be <= 3"),
-        };
-        let d0_joypad = match (self.th_flip_count, th) {
-            (0 | 1, _) | (2, true) => !joypad_state.up,
-            (3, true) => !joypad_state.z,
-            (2, false) => false,
-            (3, false) => true,
-            _ => panic!("th_flip_count should always be <= 3"),
-        };
+        // Only bits set to input come from the controller (corresponding bit in CTRL = 0)
+        controller_byte &= !self.last_ctrl_write;
 
-        let last_data_write = self.last_data_write;
-        (last_data_write & 0x80)
-            | (u8::from(th) << 6)
-            | (u8::from(self.tr.to_data_bit(tr_joypad, last_data_write.bit(5))) << 5)
-            | (u8::from(self.tl.to_data_bit(tl_joypad, last_data_write.bit(4))) << 4)
-            | (u8::from(self.right.to_data_bit(d3_joypad, last_data_write.bit(3))) << 3)
-            | (u8::from(self.left.to_data_bit(d2_joypad, last_data_write.bit(2))) << 2)
-            | (u8::from(self.down.to_data_bit(d1_joypad, last_data_write.bit(1))) << 1)
-            | u8::from(self.up.to_data_bit(d0_joypad, last_data_write.bit(0)))
+        // Bit 7 always comes from the last data write
+        let outputs_byte = self.last_data_write & (self.last_ctrl_write | 0x80);
+
+        controller_byte | outputs_byte
     }
 
     fn to_ctrl_byte(self) -> u8 {
-        (u8::from(self.th.to_ctrl_bit()) << 6)
-            | (u8::from(self.tr.to_ctrl_bit()) << 5)
-            | (u8::from(self.tl.to_ctrl_bit()) << 4)
-            | (u8::from(self.right.to_ctrl_bit()) << 3)
-            | (u8::from(self.left.to_ctrl_bit()) << 2)
-            | (u8::from(self.down.to_ctrl_bit()) << 1)
-            | u8::from(self.up.to_ctrl_bit())
+        self.last_ctrl_write
     }
 
     fn tick(&mut self, m68k_cycles: u32) {
@@ -234,11 +208,11 @@ impl InputState {
     }
 
     pub fn write_p1_ctrl(&mut self, value: u8) {
-        self.p1_pin_directions.write_ctrl(value);
+        self.p1_pin_directions.write_ctrl(value, self.p1_controller_type);
     }
 
     pub fn write_p2_ctrl(&mut self, value: u8) {
-        self.p2_pin_directions.write_ctrl(value);
+        self.p2_pin_directions.write_ctrl(value, self.p2_controller_type);
     }
 
     pub fn tick(&mut self, m68k_cycles: u32) {
