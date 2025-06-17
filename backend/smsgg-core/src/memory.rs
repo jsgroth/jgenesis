d7cfc3db1c04c40c65450acee97eeea72334d31e
jsgroth <1137683+jsgroth@users.noreply.github.com>
2025-06-17 11:50:39 -0500

    [SMS/GG] refactor mapper code to make it easier to support additional mappers

--- backend/smsgg-core/src/memory.rs
+++ backend/smsgg-core/src/memory.rs
@@ -1,19 +1,18 @@
 //! Sega Master System / Game Gear memory map
 
+mod mappers;
 mod metadata;
 
+use crate::memory::mappers::Mapper;
 use bincode::{Decode, Encode};
 use crc::Crc;
 use jgenesis_common::num::GetBit;
 use jgenesis_proc_macros::{FakeDecode, FakeEncode, PartialClone};
 use smsgg_config::SmsGgRegion;
-use std::ops::{Index, RangeInclusive};
+use std::ops::Index;
 use std::{array, mem};
 
 const SYSTEM_RAM_SIZE: usize = 8 * 1024;
-
-const CODEMASTERS_CHECKSUM_ADDR: usize = 0x7FE6;
-const SEGA_HEADER_ADDR_RANGE: RangeInclusive<usize> = 0x7FF0..=0x7FFF;
 
 // Most cartridges with RAM only had 8KB, but up to 32KB was supported, and the header contains
 // no information on RAM size (or even whether RAM is present)
@@ -21,54 +20,8 @@ const CARTRIDGE_RAM_SIZE: usize = 32 * 1024;
 
 const CRC: Crc<u32> = Crc::<u32>::new(&crc::CRC_32_ISO_HDLC);
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Default, Encode, Decode)]
-enum Mapper {
-    #[default]
-    Sega,
-    Codemasters,
-}
-
-impl Mapper {
-    // Codemasters ROMs have a 16-bit checksum at $7FE6 which is the sum of all 16-bit words in the ROM
-    // except for the words in the Sega header. If summing all of the words matches the word at
-    // $7FE6, assume this is a Codemasters ROM
-    fn detect_from_rom(rom: &[u8]) -> Self {
-        if rom.len() < 32 * 1024 {
-            // No real ROMs should be less than 32KB, but regardless this isn't a Codemasters ROM
-            return Self::Sega;
-        }
-
-        let expected_checksum = u16::from_le_bytes([
-            rom[CODEMASTERS_CHECKSUM_ADDR],
-            rom[CODEMASTERS_CHECKSUM_ADDR + 1],
-        ]);
-        let mut checksum = 0_u16;
-        for address in (0..rom.len() & !1).step_by(2) {
-            if !SEGA_HEADER_ADDR_RANGE.contains(&address) {
-                let word = u16::from_le_bytes([rom[address], rom[address + 1]]);
-                checksum = checksum.wrapping_add(word);
-            }
-        }
-
-        if checksum == expected_checksum {
-            log::info!(
-                "Codemasters-style ROM checksum is {checksum:04X}, matches word at {CODEMASTERS_CHECKSUM_ADDR:04X}; assuming this is a Codemasters ROM"
-            );
-            Self::Codemasters
-        } else {
-            Self::Sega
-        }
-    }
-}
-
 #[derive(Debug, Clone, Default, FakeEncode, FakeDecode)]
 struct Rom(Vec<u8>);
-
-impl Rom {
-    fn len(&self) -> usize {
-        self.0.len()
-    }
-}
 
 impl Index<usize> for Rom {
     type Output = <Vec<u8> as Index<usize>>::Output;
@@ -85,11 +38,6 @@ struct Cartridge {
     ram: Vec<u8>,
     mapper: Mapper,
     has_battery: bool,
-    rom_bank_0: u32,
-    rom_bank_1: u32,
-    rom_bank_2: u32,
-    ram_mapped: bool,
-    ram_bank: u32,
     ram_dirty: bool,
 }
 
@@ -114,81 +62,15 @@ impl Cartridge {
 
         jgenesis_common::rom::mirror_to_next_power_of_two(&mut rom);
 
-        Self {
-            rom: Rom(rom),
-            ram,
-            mapper,
-            has_battery,
-            rom_bank_0: 0,
-            rom_bank_1: 1,
-            rom_bank_2: 2,
-            ram_mapped: false,
-            ram_bank: 0,
-            ram_dirty: false,
-        }
-    }
-
-    fn read_rom_address(&self, address: u32) -> u8 {
-        let wrapped_addr = (address as usize) & (self.rom.len() - 1);
-        self.rom[wrapped_addr]
+        Self { rom: Rom(rom), ram, mapper, has_battery, ram_dirty: false }
     }
 
     fn read(&self, address: u16) -> u8 {
-        match (self.mapper, address) {
-            (Mapper::Sega, 0x0000..=0x03FF) => self.rom[address as usize],
-            (Mapper::Sega, 0x0400..=0x3FFF) | (Mapper::Codemasters, 0x0000..=0x3FFF) => {
-                let rom_addr = (self.rom_bank_0 << 14) | u32::from(address);
-                self.read_rom_address(rom_addr)
-            }
-            (_, 0x4000..=0x7FFF) => {
-                let rom_addr = (self.rom_bank_1 << 14) | u32::from(address & 0x3FFF);
-                self.read_rom_address(rom_addr)
-            }
-            (Mapper::Sega, 0x8000..=0xBFFF) => {
-                if self.ram_mapped {
-                    let ram_addr = (self.ram_bank << 14) | u32::from(address & 0x3FFF);
-                    self.ram[ram_addr as usize]
-                } else {
-                    let rom_addr = (self.rom_bank_2 << 14) | u32::from(address & 0x3FFF);
-                    self.read_rom_address(rom_addr)
-                }
-            }
-            (Mapper::Codemasters, 0x8000..=0x9FFF) => {
-                let rom_addr = (self.rom_bank_2 << 14) | u32::from(address & 0x3FFF);
-                self.read_rom_address(rom_addr)
-            }
-            (Mapper::Codemasters, 0xA000..=0xBFFF) => {
-                if self.ram_mapped {
-                    // Codemasters cartridges with RAM don't support banking; they only have 8KB
-                    self.ram[(address & 0x1FFF) as usize]
-                } else {
-                    let rom_addr = (self.rom_bank_2 << 14) | u32::from(address & 0x3FFF);
-                    self.read_rom_address(rom_addr)
-                }
-            }
-            _ => panic!("0xC000..=0xFFFF should never be read from cartridge"),
-        }
+        self.mapper.read(address, &self.rom.0, &self.ram)
     }
 
-    fn write_ram(&mut self, address: u16, value: u8) {
-        if self.ram_mapped {
-            let ram_addr = match self.mapper {
-                Mapper::Sega => (self.ram_bank << 14) | u32::from(address & 0x3FFF),
-                Mapper::Codemasters => (address & 0x1FFF).into(),
-            };
-            self.ram[ram_addr as usize] = value;
-
-            self.ram_dirty = true;
-        }
-    }
-
-    fn set_ram_mapped(&mut self, value: bool) {
-        self.ram_mapped = value;
-
-        if value && !self.has_battery {
-            log::info!("Cartridge RAM enabled; assuming cartridge has a battery backup");
-            self.has_battery = true;
-        }
+    fn write(&mut self, address: u16, value: u8) {
+        self.mapper.write(address, value, &mut self.ram, &mut self.ram_dirty);
     }
 }
 
@@ -330,45 +212,13 @@ impl Memory {
             self.bios_rom_banks[(address - 0xFFFD) as usize] = value.into();
         }
 
-        if !self.memory_control.cartridge_enabled {
-            return;
+        if self.memory_control.cartridge_enabled {
+            self.cartridge.write(address, value);
         }
 
-        match (self.cartridge.mapper, address) {
-            (Mapper::Sega, 0x8000..=0xBFFF) => {
-                self.cartridge.write_ram(address, value);
-            }
-            (Mapper::Sega, 0xFFFC) => {
-                log::debug!("RAM flags set to {value:02X}");
-                self.cartridge.set_ram_mapped(value.bit(3));
-                self.cartridge.ram_bank = value.bit(2).into();
-            }
-            (Mapper::Sega, 0xFFFD) | (Mapper::Codemasters, 0x0000..=0x3FFF) => {
-                log::debug!("ROM bank 0 set to {value:02X}");
-                self.cartridge.rom_bank_0 = value.into();
-            }
-            (Mapper::Sega, 0xFFFE) => {
-                log::debug!("ROM bank 1 set to {value:02X}");
-                self.cartridge.rom_bank_1 = value.into();
-            }
-            (Mapper::Sega, 0xFFFF) | (Mapper::Codemasters, 0x8000..=0x9FFF) => {
-                log::debug!("ROM bank 2 set to {value:02X}");
-                self.cartridge.rom_bank_2 = value.into();
-            }
-            (Mapper::Codemasters, 0x4000..=0x7FFF) => {
-                log::debug!("ROM bank 1 set to {value:02X}");
-                self.cartridge.rom_bank_1 = value.into();
-                self.cartridge.set_ram_mapped(value.bit(7));
-            }
-            (Mapper::Codemasters, 0xA000..=0xBFFF) => {
-                if self.cartridge.ram_mapped {
-                    self.cartridge.write_ram(address, value);
-                } else {
-                    self.cartridge.rom_bank_2 = value.into();
-                }
-            }
-            _ => {}
-        }
+        // If a game ever writes to cartridge RAM, assume it is battery-backed (since this isn't in
+        // the cartridge header)
+        self.cartridge.has_battery |= self.cartridge.ram_dirty;
     }
 
     pub fn cartridge_ram(&self) -> &[u8] {
---
+++ backend/smsgg-core/src/memory/mappers.rs
@@ -0,0 +1,208 @@
+use bincode::{Decode, Encode};
+use jgenesis_common::num::GetBit;
+use jgenesis_proc_macros::MatchEachVariantMacro;
+use std::ops::RangeInclusive;
+
+const CODEMASTERS_CHECKSUM_ADDR: usize = 0x7FE6;
+const SEGA_HEADER_ADDR_RANGE: RangeInclusive<usize> = 0x7FF0..=0x7FFF;
+
+macro_rules! invalid_cartridge_address {
+    ($address:expr) => {
+        panic!("Invalid cartridge address {:04X}, must be $0000-$BFFF", $address)
+    };
+}
+
+fn read_16kb_banked(bytes: &[u8], address: u16, bank: u32) -> u8 {
+    let rom_addr = (bank << 14) | u32::from(address & 0x3FFF);
+    read_wrapped(bytes, rom_addr)
+}
+
+fn read_wrapped(bytes: &[u8], address: u32) -> u8 {
+    let wrapped_addr = (address as usize) & (bytes.len() - 1);
+    bytes[wrapped_addr]
+}
+
+fn write_16kb_banked(bytes: &mut [u8], address: u16, bank: u32, value: u8) {
+    let rom_addr = (bank << 14) | u32::from(address & 0x3FFF);
+    write_wrapped(bytes, rom_addr, value);
+}
+
+fn write_wrapped(bytes: &mut [u8], address: u32, value: u8) {
+    let wrapped_addr = (address as usize) & (bytes.len() - 1);
+    bytes[wrapped_addr] = value;
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
+pub struct SegaMapper {
+    rom_banks: [u32; 3],
+    ram_bank: u32,
+    ram_enabled: bool,
+}
+
+impl SegaMapper {
+    pub fn new() -> Self {
+        Self { rom_banks: [0, 1, 2], ram_bank: 0, ram_enabled: false }
+    }
+
+    pub fn read(&self, address: u16, rom: &[u8], ram: &[u8]) -> u8 {
+        match address {
+            0x0000..=0x03FF => {
+                // First 1KB of ROM
+                read_wrapped(rom, address.into())
+            }
+            0x0400..=0x7FFF => {
+                // Mappable ROM banks 0/1
+                let bank = self.rom_banks[(address / 0x4000) as usize];
+                read_16kb_banked(rom, address, bank)
+            }
+            0x8000..=0xBFFF => {
+                // Mappable ROM bank 2 or RAM bank
+                if self.ram_enabled {
+                    read_16kb_banked(ram, address, self.ram_bank)
+                } else {
+                    read_16kb_banked(rom, address, self.rom_banks[2])
+                }
+            }
+            0xC000..=0xFFFF => invalid_cartridge_address!(address),
+        }
+    }
+
+    pub fn write(&mut self, address: u16, value: u8, ram: &mut [u8], ram_dirty: &mut bool) {
+        match address {
+            0x8000..=0xBFFF => {
+                // RAM bank (if mapped)
+                if self.ram_enabled {
+                    write_16kb_banked(ram, address, self.ram_bank, value);
+                    *ram_dirty = true;
+                }
+            }
+            0xFFFC => {
+                // Control / RAM banking
+                self.ram_bank = value.bit(2).into();
+                self.ram_enabled = value.bit(3);
+                // TODO Bank shifting and RAM enabled for $C000-$FFFF; not used by any known software
+                log::debug!(
+                    "$FFFC write: {value:02X} (RAM bank = {}, RAM enabled = {})",
+                    self.ram_bank,
+                    self.ram_enabled
+                );
+            }
+            0xFFFD..=0xFFFF => {
+                // ROM banking
+                let bank_idx = address - 0xFFFD;
+                self.rom_banks[bank_idx as usize] = value.into();
+                log::debug!("${address:02X} write: {value:02X} (ROM bank {bank_idx})");
+            }
+            _ => {}
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
+pub struct CodemastersMapper {
+    rom_banks: [u32; 3],
+    ram_enabled: bool,
+}
+
+impl CodemastersMapper {
+    pub fn new() -> Self {
+        Self { rom_banks: [0, 1, 2], ram_enabled: false }
+    }
+
+    pub fn read(&self, address: u16, rom: &[u8], ram: &[u8]) -> u8 {
+        match address {
+            0x0000..=0x9FFF => {
+                // ROM banks (16KB)
+                let bank = self.rom_banks[(address / 0x4000) as usize];
+                read_16kb_banked(rom, address, bank)
+            }
+            0xA000..=0xBFFF => {
+                // RAM or upper half of ROM bank 2
+                if self.ram_enabled {
+                    // No Codemasters game has more than 8KB of RAM
+                    read_wrapped(ram, (address & 0x1FFF).into())
+                } else {
+                    read_16kb_banked(rom, address, self.rom_banks[2])
+                }
+            }
+            0xC000..=0xFFFF => invalid_cartridge_address!(address),
+        }
+    }
+
+    pub fn write(&mut self, address: u16, value: u8, ram: &mut [u8], ram_dirty: &mut bool) {
+        match address {
+            0x0000..=0x3FFF => {
+                // ROM bank 0
+                self.rom_banks[0] = value.into();
+                log::trace!("${address:04X} write: {value:02X} (ROM bank 0)");
+            }
+            0x4000..=0x7FFF => {
+                // ROM bank 1 and RAM enabled
+                self.rom_banks[1] = value.into();
+                self.ram_enabled = value.bit(7);
+                log::trace!(
+                    "${address:04X} write: {value:02X} (ROM bank 1, RAM enabled = {})",
+                    self.ram_enabled
+                );
+            }
+            0x8000..=0xBFFF => {
+                // ROM bank 2 and RAM (in upper half if enabled)
+                if self.ram_enabled && address >= 0xA000 {
+                    write_wrapped(ram, (address & 0x1FFF).into(), value);
+                    *ram_dirty = true;
+                } else {
+                    self.rom_banks[2] = value.into();
+                    log::trace!("${address:04X} write: {value:02X} (ROM bank 2)");
+                }
+            }
+            0xC000..=0xFFFF => {}
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Encode, Decode, MatchEachVariantMacro)]
+pub enum Mapper {
+    Sega(SegaMapper),
+    Codemasters(CodemastersMapper),
+}
+
+impl Mapper {
+    // Codemasters ROMs have a 16-bit checksum at $7FE6 which is the sum of all 16-bit words in the ROM
+    // except for the words in the Sega header. If summing all of the words matches the word at
+    // $7FE6, assume this is a Codemasters ROM
+    pub fn detect_from_rom(rom: &[u8]) -> Self {
+        if rom.len() < 32 * 1024 {
+            // No real ROMs should be less than 32KB, but regardless this isn't a Codemasters ROM
+            return Self::Sega(SegaMapper::new());
+        }
+
+        let expected_checksum = u16::from_le_bytes([
+            rom[CODEMASTERS_CHECKSUM_ADDR],
+            rom[CODEMASTERS_CHECKSUM_ADDR + 1],
+        ]);
+        let mut checksum = 0_u16;
+        for address in (0..rom.len() & !1).step_by(2) {
+            if !SEGA_HEADER_ADDR_RANGE.contains(&address) {
+                let word = u16::from_le_bytes([rom[address], rom[address + 1]]);
+                checksum = checksum.wrapping_add(word);
+            }
+        }
+
+        if checksum == expected_checksum {
+            log::info!(
+                "Codemasters-style ROM checksum is {checksum:04X}, matches word at {CODEMASTERS_CHECKSUM_ADDR:04X}; assuming this is a Codemasters ROM"
+            );
+            Self::Codemasters(CodemastersMapper::new())
+        } else {
+            Self::Sega(SegaMapper::new())
+        }
+    }
+
+    pub fn read(&self, address: u16, rom: &[u8], ram: &[u8]) -> u8 {
+        match_each_variant!(self, mapper => mapper.read(address, rom, ram))
+    }
+
+    pub fn write(&mut self, address: u16, value: u8, ram: &mut [u8], ram_dirty: &mut bool) {
+        match_each_variant!(self, mapper => mapper.write(address, value, ram, ram_dirty));
+    }
+}
