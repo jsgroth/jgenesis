fed9aa1eb5d52a68b1a6fce3a76a2ace445a470a
jsgroth <1137683+jsgroth@users.noreply.github.com>
2025-09-04 21:11:28 -0500

    make FakeEncode and FakeDecode proc macros able to handle types with generic parameters

--- common/jgenesis-proc-macros/src/encode.rs
+++ common/jgenesis-proc-macros/src/encode.rs
@@ -1,13 +1,16 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::DeriveInput;
+use proc_macro2::{Ident, Span};
+use quote::{format_ident, quote};
+use syn::{DeriveInput, GenericParam, Lifetime, LifetimeParam, TypeParam};
 
 pub fn fake_encode(input: TokenStream) -> TokenStream {
     let input: DeriveInput = syn::parse(input).expect("Unable to parse input");
 
+    let (impl_generics, type_generics, where_clause) = input.generics.split_for_impl();
+
     let type_ident = &input.ident;
     let expanded = quote! {
-        impl ::bincode::Encode for #type_ident {
+        impl #impl_generics ::bincode::Encode for #type_ident #type_generics #where_clause {
             fn encode<E: ::bincode::enc::Encoder>(
                 &self,
                 _encoder: &mut E
@@ -23,9 +26,25 @@ pub fn fake_encode(input: TokenStream) -> TokenStream {
 pub fn fake_decode(input: TokenStream) -> TokenStream {
     let input: DeriveInput = syn::parse(input).expect("Unable to parse input");
 
+    let (_, type_generics, where_clause) = input.generics.split_for_impl();
+
+    let mut decode_generics = input.generics.clone();
+    decode_generics.params.insert(0, GenericParam::Type(TypeParam::from(format_ident!("Context"))));
+    let (decode_impl_generics, _, _) = decode_generics.split_for_impl();
+
+    let mut borrow_generics = input.generics.clone();
+    borrow_generics
+        .params
+        .insert(0, GenericParam::Type(TypeParam::from(Ident::new("Context", Span::call_site()))));
+    borrow_generics.params.insert(
+        0,
+        GenericParam::Lifetime(LifetimeParam::new(Lifetime::new("'de", Span::call_site()))),
+    );
+    let (borrow_impl_generics, _, _) = borrow_generics.split_for_impl();
+
     let type_ident = &input.ident;
     let expanded = quote! {
-        impl<Context> ::bincode::Decode<Context> for #type_ident {
+        impl #decode_impl_generics ::bincode::Decode<Context> for #type_ident #type_generics #where_clause {
             fn decode<D: ::bincode::de::Decoder<Context = Context>>(
                 _decoder: &mut D
             ) -> ::std::result::Result<Self, ::bincode::error::DecodeError> {
@@ -33,7 +52,7 @@ pub fn fake_decode(input: TokenStream) -> TokenStream {
             }
         }
 
-        impl<'de, Context> ::bincode::BorrowDecode<'de, Context> for #type_ident {
+        impl #borrow_impl_generics ::bincode::BorrowDecode<'de, Context> for #type_ident #type_generics #where_clause {
             fn borrow_decode<D: ::bincode::de::BorrowDecoder<'de, Context = Context>>(
                 _decoder: &mut D
             ) -> ::std::result::Result<Self, ::bincode::error::DecodeError> {
--- common/jgenesis-proc-macros/tests/encode.rs
+++ common/jgenesis-proc-macros/tests/encode.rs
@@ -26,3 +26,28 @@ fn fake_encode_decode() {
             .expect("Failed to deserialize using FakeDecode implementation");
     assert!(deserialized.file.is_none());
 }
+
+#[derive(FakeEncode, FakeDecode)]
+struct HasGenericType<T: Default>(T);
+
+impl<T: Default> Default for HasGenericType<T> {
+    fn default() -> Self {
+        Self(T::default())
+    }
+}
+
+#[test]
+fn fake_encode_decode_with_generics() {
+    let bincode_config = bincode::config::standard();
+
+    let value: HasGenericType<i32> = HasGenericType(42);
+
+    let serialized = bincode::encode_to_vec(value, bincode_config)
+        .expect("Failed to serialize using FakeEncode implementation");
+
+    let (deserialized, _) =
+        bincode::decode_from_slice::<HasGenericType<i32>, _>(&serialized, bincode_config)
+            .expect("Failed to deserialize using FakeDecode implementation");
+
+    assert_eq!(deserialized.0, 0);
+}
