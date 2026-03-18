57d868668f177603d406e3d0364953a6cff31e17
jsgroth <1137683+jsgroth@users.noreply.github.com>
2026-03-18 12:04:15 -0500

    Z80 disassembly UI for Gen/SCD/32X
    currently has the same limitations as the 68000 disassembly UI

--- cpu/z80-emu/src/lib.rs
+++ cpu/z80-emu/src/lib.rs
@@ -3,5 +3,5 @@ mod disassemble;
 pub mod traits;
 
 pub use core::{Flags, InterruptMode, Registers, Z80};
-pub use disassemble::disassemble_into;
+pub use disassemble::{DisassembledInstruction, disassemble_into};
 pub use traits::BusInterface;
